ec6758bdf67f5f6dbab88339086284d751cd8b90
Anderson Prado <andeonbr@gmail.com>
2025-04-08 13:26:56 -0300

    back original

--- toonz/sources/include/tmachine.h
+++ toonz/sources/include/tmachine.h
@@ -3,7 +3,6 @@
 #ifndef T_MACHINE_INCLUDED
 #define T_MACHINE_INCLUDED
 
-// Platform-specific channel order definitions
 #if defined(_WIN32) || defined(i386)
 #define TNZ_MACHINE_CHANNEL_ORDER_BGRM 1
 #elif defined(__sgi)
@@ -13,12 +12,11 @@
 #elif defined(MACOSX)
 #define TNZ_MACHINE_CHANNEL_ORDER_MRGB 1
 #else
-#error "Unknown platform - cannot determine channel order"
+@UNKNOWN PLATFORM @
 #endif
 
-// Endianness check - now properly handled by CMake
-#ifndef TNZ_LITTLE_ENDIAN
-#error "TNZ_LITTLE_ENDIAN must be defined (0 for big-endian, 1 for little-endian)"
+#if !defined(TNZ_LITTLE_ENDIAN)
+#error "TNZ_LITTLE_ENDIAN not defined!"
 #endif
 
 #endif
